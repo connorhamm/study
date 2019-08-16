8479dd4f3e2633ed157137cbcd9f19f8305d929a
Connor Hamm <Connor_Hamm@jabil.com>
2019-08-16 16:01:45 -0700

    added support to windows

--- study.py
+++ study.py
@@ -7,31 +7,43 @@ Purpose: Open resources for studying
 import webbrowser
 import os
 
-websites = {'Python Bootcamp' : "https://www.udemy.com/complete-python-bootcamp/",
-            'Tomato Timer' : "https://tomato-timer.com/",
-            'One Note' : "https://jabil-my.sharepoint.com/personal/connor_hamm_jabil_com/_layouts/15/WopiFrame.aspx?sourcedoc={395547fd-b437-4c4c-b385-752e4eafb2f2}&action=edit&wd=target%28Classes.one%7Ca49583cb-bba5-41a2-b5b2-66125086bafc%2F%5BUdemy%5D%20Python%20Bootcamp%7Cc47c1968-6e3c-495f-bcc2-73ac2ee93678%2F%29"
-            }
-
-def open_websites():
-    for key, value in websites.items():
-        print("Opening - {}".format(key))
-        webbrowser.open(value)
+def open_websites(website):
+        webbrowser.open(website)
 
 def open_apps(): 
     os.system("nohup spotify &")
     os.system("nohup anaconda-navigator &")
 
-def update_repo():
+def update_repo(system):
     update = input("Update Repository? (y/n)")
     if update == 'y':
-        os.chdir('/home/connor/Classes/Complete-Python-3-Bootcamp')
-        os.system("git fetch --all")
-        os.system("git reset --hard origin/master")
-        os.chdir('/home/')
-    else:
-        print("Update Aborted")
+        if system == 'w':
+            os.chdir('C:/Users/100042090/OneDrive - Jabil/Personal/MyCode/Complete-Python-3-Bootcamp')
+            os.system("git push")
+        elif system == 'l':
+            os.chdir('/home/connor/Classes/Complete-Python-3-Bootcamp')
+            os.system("git push")
+        else:
+            print("OS not support")
 
-update_repo()
-open_websites()
-open_apps()
+# update_repo()
+system = input("(w)indows or (l)inux? ")
+update_repo(system)
 
+if system == 'w':
+    # Open Websites
+    webbrowser.open("https://www.udemy.com/complete-python-bootcamp/")
+    webbrowser.open("https://tomato-timer.com/")
+
+elif system == 'l':
+    # Open Websites
+    webbrowser.open("https://www.udemy.com/complete-python-bootcamp/")     
+    webbrowser.open("https://tomato-timer.com/")
+    webbrowser.open("https://jabil-my.sharepoint.com/personal/connor_hamm_jabil_com/_layouts/15/WopiFrame.aspx?sourcedoc={395547fd-b437-4c4c-b385-752e4eafb2f2}&action=edit&wd=target%28Classes.one%7Ca49583cb-bba5-41a2-b5b2-66125086bafc%2F%5BUdemy%5D%20Python%20Bootcamp%7Cc47c1968-6e3c-495f-bcc2-73ac2ee93678%2F%29")
+
+    # Open Apps
+    os.system("nohup spotify &")     
+    os.system("nohup anaconda-navigator &")
+
+else:
+    print("OS not supported")
